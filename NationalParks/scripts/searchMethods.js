708fc4ab019ffe6ea4732193294bbf135b333552
ltejedajr <126844440+ltejedajr@users.noreply.github.com>
2023-11-16 20:31:50 -0500

    Completed park search page

--- NationalParks/scripts/searchMethods.js
+++ NationalParks/scripts/searchMethods.js
@@ -1,97 +1,202 @@
 "use strict";
 
+const parkContainer = document.getElementById("parkContainer");
+const locationRadioBtn = document.getElementById("locationRadioBtn");
+const parkTypeRadioBtn = document.getElementById("parkTypeRadioBtn");
+const locationDropdown = document.getElementById("locationDropdown");
+const parkTypeDropdown = document.getElementById("parkTypeDropdown");
+
 window.onload = init;
 
 function init() {
-
-    // Declared new variables for radio elements from HTML document
-    const locationRadioBtn = document.getElementById("locationRadioBtn");
-    const parkTypeRadioBtn = document.getElementById("parkTypeRadioBtn");
-
-    // Declared new variables for dropdown elements from HTML document
-    const locationDropdown = document.getElementById("locationDropdown");
-    const parkTypeDropdown = document.getElementById("parkTypeDropdown");
-
-    // Set click event handlers to control dropdown options
-    locationRadioBtn.onclick = function () {
-        locationDropdown.style.display = "block";
-        parkTypeDropdown.style.display = "none"; // Hides parkTypeDropdown if locationRadioBtn is checked
-
-        for (let states of locationsArray) {
-            let option = document.createElement("option");
-            option.innerText = states;
-            locationDropdown.appendChild(option);
-        }
-    };
-
-    parkTypeRadioBtn.onclick = function () {
-        parkTypeDropdown.style.display = "block";
-        locationDropdown.style.display = "none"; // Hides locationDropdown if parkTypeRadioBtn is checked
-
-        for (let parkTypes of parkTypesArray) {
-            let option = document.createElement("option");
-            option.innerText = parkTypes;
-            parkTypeDropdown.appendChild(option);
-
-        }
-    };
+    locationRadioBtn.onclick = handleRadioClicked;
+    parkTypeRadioBtn.onclick = handleRadioClicked;
+    locationDropdown.onchange = dropdownSelection;
+    parkTypeDropdown.onchange = dropdownSelection;
 };
 
-function populateParkInfo(locationDropdown, parkTypeDropdown) {
+function handleRadioClicked() {
+    clearContainer();
 
-    const nationalParkDetails = document.getElementById("nationalParkDetails");
-    const locationId = document.getElementById("locationId");
-    const locationName = document.getElementById("locationName");
-    const locationAddress = document.getElementById("locationAddress");
-    const locationCity = document.getElementById("locationCity");
-    const locationState = document.getElementById("locationState");
-    const locationZipCode = document.getElementById("locationZipCode");
-    const locationPhone = document.getElementById("locationPhone");
-    const locationFax = document.getElementById("locationFax");
-    const locationLatitude = document.getElementById("locationLatitude");
-    const locationLongitude = document.getElementById("locationLongitude");
-    const locationCoordinates = document.getElementById("locationCoordinates");
-
-    const selectedState = locationDropdown.value;
-    const selectedParkType = parkTypeDropdown.value;
-
-    locationDropdown.onchange = function () {
-    if (selectedState !== "") {
-        let selectedLocation = nationalParksArray.find(park => park.State == selectedState);
-        locationId.innerText = selectedLocation.LocationID;
-        locationName.innerText = selectedLocation.LocationName;
-        locationAddress.innerText = selectedLocation.Address;
-        locationCity.innerText = selectedLocation.City;
-        locationState.innerText = selectedLocation.State;
-        locationZipCode.innerText = selectedLocation.ZipCode;
-        locationPhone.innerText = selectedLocation.Phone;
-        locationFax.innerText = selectedLocation.Fax;
-        locationLatitude.innerText = selectedLocation.Latitude;
-        locationLongitude.innerText = selectedLocation.Longitude;
-        locationCoordinates.innerText = selectedLocation.Location;
-
-        nationalParkDetails.style.display = "block"
+    if (locationRadioBtn.checked) {
+        showLocationDropdown();
     }
-};
-    parkTypeDropdown.onchange = function () {
-
-    if (selectedParkType !== "") {
-        let selectedType = nationalParksArray.find(park => park.LocationID == selectedParkType);
-        locationId.innerText = selectedType.LocationID;
-        locationName.innerText = selectedType.LocationName;
-        locationAddress.innerText = selectedType.Address;
-        locationCity.innerText = selectedType.City;
-        locationState.innerText = selectedType.State;
-        locationZipCode.innerText = selectedType.ZipCode;
-        locationPhone.innerText = selectedType.Phone;
-        locationFax.innerText = selectedType.Fax;
-        locationLatitude.innerText = selectedType.Latitude;
-        locationLongitude.innerText = selectedType.Longitude;
-        locationCoordinates.innerText = selectedType.Location;
-
-        nationalParkDetails.style.display = "block"
+    else if (parkTypeRadioBtn.checked) {
+        showParkTypeDropdown();
     }
 };
 
+function showLocationDropdown() {
+    locationDropdown.style.display = "block";
+    parkTypeDropdown.style.display = "none";
+
+    let defaultOption = document.getElementById("defaultLocationOption");
+    defaultOption.innerText = "Select a state";
+
+    for (let state of locationsArray) {
+        let option = document.createElement("option");
+        option.value = state;
+        option.innerText = state;
+        locationDropdown.appendChild(option);
+    }
+    locationDropdown.value = "";
 };
 
+function showParkTypeDropdown() {
+    parkTypeDropdown.style.display = "block";
+    locationDropdown.style.display = "none";
+
+    let defaultOption = document.getElementById("defaultTypeOption");
+    defaultOption.innerText = "Select a type of park";
+    
+    for (let parkType of parkTypesArray) {
+        let option = document.createElement("option");
+        option.value = parkType;
+        option.innerText = parkType;
+        parkTypeDropdown.appendChild(option);
+    }
+    parkTypeDropdown.value = "";
+};
+
+function dropdownSelection() {
+    clearContainer();
+
+    if (locationRadioBtn.checked) {
+        const stateSelection = locationDropdown.value;
+        const parksList = filterParksByState(stateSelection);
+        parkInfoByStateContainer(parksList);
+    }
+    else if (parkTypeRadioBtn.checked) {
+        const parkTypeSelection = parkTypeDropdown.value;
+        const typeList = filterParksByType(parkTypeSelection);
+        parkInfoByTypeContainer(typeList);
+    }
+};
+
+function filterParksByState(state) {
+    return nationalParksArray.filter(natPark => natPark.State == state);
+};
+
+function filterParksByType(type) {
+    return nationalParksArray.filter(natPark => natPark.LocationName.includes(type));
+};
+
+function parkInfoByStateContainer(parksList) {
+    parksList.forEach(park => {
+        let accordionItemDiv = document.createElement("div");
+        accordionItemDiv.className = "accordion-item";
+
+        parkContainer.appendChild(accordionItemDiv);
+
+        let accordionHeader = document.createElement("h2");
+        accordionHeader.className = "accordion-header";
+
+        accordionItemDiv.appendChild(accordionHeader);
+
+        let btn = document.createElement("button");
+        btn.className = "accordion-button collapsed";
+        btn.type = "button";
+        btn.setAttribute("data-bs-toggle", "collapse");
+
+        let targetId = "flush-collapse-" + park.LocationID;
+
+        btn.setAttribute("data-bs-target", "#" + targetId);
+        btn.setAttribute("aria-expanded", "false");
+        btn.setAttribute("aria-controls", targetId);
+
+        let btnTextNode = document.createTextNode(park.LocationName);
+        btn.appendChild(btnTextNode);
+
+        accordionHeader.appendChild(btn);
+
+        let flushCollapseDiv = document.createElement("div");
+        flushCollapseDiv.id = targetId;
+        flushCollapseDiv.className = "accordion-collapse collapse"
+        flushCollapseDiv.setAttribute("data-bs-parent", "#parkContainer");
+
+        let accordionBody = document.createElement("div");
+        accordionBody.className = "accordion-body";
+
+        let accordionBodyHTML = `
+            <p><strong>Location ID:</strong> ${park.LocationID}</p>
+            <p><strong>Location Name:</strong> ${park.LocationName}</p>
+            <p><strong>Address:</strong> ${park.Address}</p>
+            <p><strong>City:</strong> ${park.City}</p>
+            <p><strong>State:</strong> ${park.State}</p>
+            <p><strong>Zip Code:</strong> ${park.ZipCode}</p>
+            <p><strong>Phone:</strong> ${park.Phone}</p>
+            <p><strong>Fax:</strong> ${park.Fax}</p>
+            <p><strong>Latitude:</strong> ${park.Latitude}</p>
+            <p><strong>Longitude:</strong> ${park.Longitude}</p>
+            <p><strong>Visit:</strong> ${park.hasOwnProperty('Visit')}</p>
+        `;
+
+        accordionBody.innerHTML = accordionBodyHTML;
+
+        flushCollapseDiv.appendChild(accordionBody);
+
+        accordionItemDiv.appendChild(flushCollapseDiv);
+    });
+};
+
+function parkInfoByTypeContainer(typeList) {
+    typeList.forEach(park => {
+        let accordionItemDiv = document.createElement("div");
+        accordionItemDiv.className = "accordion-item";
+
+        parkContainer.appendChild(accordionItemDiv);
+
+        let accordionHeader = document.createElement("h2");
+        accordionHeader.className = "accordion-header";
+
+        accordionItemDiv.appendChild(accordionHeader);
+
+        let btn = document.createElement("button");
+        btn.className = "accordion-button collapsed";
+        btn.type = "button";
+        btn.setAttribute("data-bs-toggle", "collapse");
+
+        let targetId = "flush-collapse-" + park.LocationID;
+
+        btn.setAttribute("data-bs-target", "#" + targetId);
+        btn.setAttribute("aria-expanded", "false");
+        btn.setAttribute("aria-controls", targetId);
+
+        let btnTextNode = document.createTextNode(park.LocationName);
+        btn.appendChild(btnTextNode);
+
+        accordionHeader.appendChild(btn);
+
+        let flushCollapseDiv = document.createElement("div");
+        flushCollapseDiv.id = targetId;
+        flushCollapseDiv.className = "accordion-collapse collapse"
+        flushCollapseDiv.setAttribute("data-bs-parent", "#parkContainer");
+
+        let accordionBody = document.createElement("div");
+        accordionBody.className = "accordion-body";
+
+        let accordionBodyHTML = `
+            <p><strong>Location ID:</strong> ${park.LocationID}</p>
+            <p><strong>Location Name:</strong> ${park.LocationName}</p>
+            <p><strong>Address:</strong> ${park.Address}</p>
+            <p><strong>City:</strong> ${park.City}</p>
+            <p><strong>State:</strong> ${park.State}</p>
+            <p><strong>Zip Code:</strong> ${park.ZipCode}</p>
+            <p><strong>Phone:</strong> ${park.Phone}</p>
+            <p><strong>Fax:</strong> ${park.Fax}</p>
+            <p><strong>Latitude:</strong> ${park.Latitude}</p>
+            <p><strong>Longitude:</strong> ${park.Longitude}</p>
+            <p><strong>Visit:</strong> ${park.hasOwnProperty('Visit')}</p>
+        `;
+
+        accordionBody.innerHTML = accordionBodyHTML;
+
+        flushCollapseDiv.appendChild(accordionBody);
+
+        accordionItemDiv.appendChild(flushCollapseDiv);
+    });
+};
+
+function clearContainer() {
+    parkContainer.innerHTML = "";
+};
